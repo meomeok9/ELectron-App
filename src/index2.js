60ab9577a82134a682ed812af3babdf8945f6ef3
meomeok9 <118091573+meomeok9@users.noreply.github.com>
2023-05-25 03:33:45 +0700

    3st

--- src/index2.js
+++ src/index2.js
@@ -1,7 +1,14 @@
 const { app , ipcMain, BrowserWindow, Menu} = require("electron");
 const path = require('path');
 let mainWindow,subWindow,mainPlasform;
-
+const mainObjWd ={
+    window:mainWindow,
+    isMain:true,
+}
+const subObjWd ={
+    window:subWindow,
+    isMain:false,
+}
 const createWindow =(window,page = null,isGotMenu = false,height=600,width=800,title='window',openDevTools = process.env.NODE_ENV !=='production')=>{
     let mainMenu;
     window.window = new BrowserWindow({
@@ -18,6 +25,7 @@ const createWindow =(window,page = null,isGotMenu = false,height=600,width=800,t
       if(openDevTools) menuTemplate.push({
         label:"View",
         submenu:[
+            {role :"reload"},
             {
                 label:'Toggle Deverloper Tools',
                 accelerator: mainPlasform==='darwin'? 'Command+Alt+I':'ctrl+shift+i',
@@ -33,12 +41,19 @@ const createWindow =(window,page = null,isGotMenu = false,height=600,width=800,t
      }
      if(window.isMain)
         window.window.on('closed',()=>app.quit());
-   
+    //don rac
+     window.window.on('closed',()=>window.window = null);
  }
+
+ ipcMain.on('todo:add',(event,todo)=>{
+    console.log('#1 todo : ',todo);
+    mainObjWd.window.webContents.send('todo:add',todo);
+    subObjWd.window.close();
+ });
 
 app.on('ready',()=>{
     mainPlasform = process.platform;
-    createWindow({window:mainWindow,isMain:true},'index2.html',true);
+    createWindow(mainObjWd,'index2.html',true);
    
 });
 
@@ -49,7 +64,13 @@ const menuTemplate =[
             {
                 label: 'New Todo',
                 click(){
-                    createWindow({window:subWindow,isMain:false},'addTodo.html',false,300,400,'Add Todo');
+                    createWindow(subObjWd,'addTodo.html',false,300,400,'Add Todo');
+                }
+            },
+            {
+                label: 'Clear Todo',
+                click(){
+                    mainObjWd.window.send('todo:clear');
                 }
             },
             {
